412e044c74f0c1f01eb76555d14d2c067e20d6b9
antoine311200 <antoine311200@gmail.com>
2025-09-22 12:10:22 +0200

    Creation of Ryuushi's repository

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="ryuushi",
+    version="1.0.0",
+    author="Antoine Debouchage",
+    author_email="antoine311200@gmail.com",
+    packages=["ryuushi"],
+)
